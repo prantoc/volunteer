a1846057a685b4acebc24b46eb69ade3e969cda5
Pranto <chakrabortypranto39@gmail.com>
2021-11-22 14:06:00 +0600

    route file

--- routes/web.php
+++ routes/web.php
@@ -15,9 +15,25 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', 'App\Http\Controllers\HomeController@index')->name('home');
-Route::get('news', 'App\Http\Controllers\HomeController@news')->name('news');
-Route::get('register', 'App\Http\Controllers\HomeController@register')->name('register');
-Route::get('single-news', 'App\Http\Controllers\HomeController@singleNews')->name('single-news');
+
+Route::get('events', 'App\Http\Controllers\HomeController@news')->name('news');
+
+Route::get('volunteer-story', 'App\Http\Controllers\HomeController@volunteerStory')->name('volunteer-story');
+
+Route::get('single-news/{slug}', 'App\Http\Controllers\HomeController@singleNews')->name('single-news');
+
 Route::get('volunteer', 'App\Http\Controllers\HomeController@volunteer')->name('volunteer');
-Route::get('volunteer-members', 'App\Http\Controllers\HomeController@volunteerMembers')->name('volunteer-members');
-Route::get('volunteer-profile', 'App\Http\Controllers\HomeController@volunteerProfile')->name('volunteer-profile');
+
+Route::get('volunteer-members/{slug}', 'App\Http\Controllers\HomeController@volunteerMembers')->name('volunteer-members');
+
+Route::get('volunteer-profile/{slug}', 'App\Http\Controllers\HomeController@volunteerProfile')->name('volunteer-profile');
+
+Route::get('register', 'App\Http\Controllers\HomeController@register')->name('register');
+
+Route::get('/search/', 'App\Http\Controllers\HomeController@search')->name('search');
+
+
+
+Route::group(['prefix' => 'admin'], function () {
+    Voyager::routes();
+});
