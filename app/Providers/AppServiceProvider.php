c4b39583cd83d6abab02064fcdfc28df2ce0811e
Pranto <chakrabortypranto39@gmail.com>
2021-11-22 13:57:16 +0600

    showing data in the footer area

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -3,6 +3,7 @@
 namespace App\Providers;
 
 use Illuminate\Support\ServiceProvider;
+use TCG\Voyager\Models\Post;
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -23,6 +24,11 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot()
     {
-        //
+        view()->composer(['layouts.footer'], function ($view) {
+            // $data['maintanance'] = \App\Setting::find(1);
+            // $data['settings'] = \App\Setting::find(2);
+            $data['posts'] = \TCG\Voyager\Models\Post::whereStatus('PUBLISHED')->whereFeatured(1)->orderBy('id','desc')->take(4)->get();
+            return $view->with( $data);
+        });
     }
 }
